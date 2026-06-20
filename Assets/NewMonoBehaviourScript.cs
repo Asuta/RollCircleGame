d4c3539b465b03c26bdb58d9a7f0232f640136a9
TOTHEMOON\youdo <1023571809@qq.com>
2026-06-20 11:44:30 +0800

    初始化 Unity 项目

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Numerics;
+using System.Transactions;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Vector3 offset; // Offset from the target position
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
