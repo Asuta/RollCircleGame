0ae0a6538fdb374b401e551428f9a828842f7f31
TOTHEMOON\youdo <1023571809@qq.com>
2026-06-25 22:09:08 +0800

    添加RayThree预制件和SweepLaserCreator脚本
    - 创建了一个名为RayThree的新预制件，其相关组件包括Transform，MeshFilter，MeshRenderer，BoxCollider和自定义MonoBehaviour。
    - 为RayThree预制件添加了元数据。
    - 已实现新脚本SweepLaserCreator，以管理场景中SweepLaser实例的创建。

---
+++ Assets/Scripts/Creator/SweepLaserCreator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SweepLaserCreator : MonoBehaviour
+{
+    public GameObject SweepLaserPrefab;
+    public Transform planeTransform;
+    public Transform SweepLaserParent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
