80303bcc35a14f63f7c66a61a529731db220bac1
TOTHEMOON\youdo <1023571809@qq.com>
2026-06-28 11:18:01 +0800

    feat: 添加 Car 类及其与玩家的碰撞检测逻辑

--- Assets/Scripts/Creator/CarCreator.cs
+++ Assets/Scripts/Creator/CarCreator.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarCreator : MonoBehaviour, IGroundTrapHandler
@@ -11,8 +12,28 @@ public class CarCreator : MonoBehaviour, IGroundTrapHandler
     public float yOffset;
     [SerializeField] private int spawnCount = 3;
     [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private float carMoveDelay = 2f;
+    [SerializeField] private float carMoveSpeed = 5f;
+    [SerializeField] private float carMoveDistanceOffset = 0f;
 
     private Coroutine spawnCoroutine;
+    private readonly Dictionary<Car, SpawnedCar> spawnedCars = new Dictionary<Car, SpawnedCar>();
+
+    private class SpawnedCar
+    {
+        public GameObject CarObject;
+        public GameObject GolineObject;
+    }
+
+    private void OnEnable()
+    {
+        Car.HitPlayer += OnCarHitPlayer;
+    }
+
+    private void OnDisable()
+    {
+        Car.HitPlayer -= OnCarHitPlayer;
+    }
 
     [InspectorButton]
     private void CreateCar()
@@ -56,7 +77,10 @@ public class CarCreator : MonoBehaviour, IGroundTrapHandler
             {
                 GameObject car = Instantiate(CarPrefab, spawnPosition, CarPrefab.transform.rotation, CarParent);
                 FacePlaneCenter(car.transform);
-                CreateGoline(car.transform);
+                GameObject goLine = CreateGoline(car.transform);
+                Car carComponent = car.GetComponentInChildren<Car>();
+                RegisterSpawnedCar(carComponent, car, goLine);
+                StartCoroutine(MoveCarAfterDelay(carComponent, car, goLine));
                 yield break;
             }
 
@@ -96,15 +120,96 @@ public class CarCreator : MonoBehaviour, IGroundTrapHandler
         target.rotation = Quaternion.LookRotation(directionToCenter, Vector3.up);
     }
 
-    private void CreateGoline(Transform carTransform)
+    private GameObject CreateGoline(Transform carTransform)
     {
         if (GolinePrefab == null || Plane == null || carTransform == null)
-            return;
+            return null;
 
         Vector3 goLinePosition = Plane.position;
         goLinePosition.y = Plane.position.y + yOffset;
         GameObject goLine = Instantiate(GolinePrefab, goLinePosition, carTransform.rotation, CarParent);
         SetGolineLength(goLine.transform, Plane.lossyScale.x);
+        return goLine;
+    }
+
+    private IEnumerator MoveCarAfterDelay(Car carComponent, GameObject car, GameObject goLine)
+    {
+        yield return new WaitForSeconds(carMoveDelay);
+
+        if (car == null || Plane == null)
+            yield break;
+
+        Transform carTransform = car.transform;
+        Vector3 moveDirection = carTransform.forward;
+        moveDirection.y = 0f;
+
+        if (moveDirection == Vector3.zero)
+            yield break;
+
+        moveDirection.Normalize();
+
+        float targetDistance = Plane.lossyScale.x + carMoveDistanceOffset;
+        float movedDistance = 0f;
+
+        while (movedDistance < targetDistance)
+        {
+            if (car == null)
+                yield break;
+
+            float moveDistance = Mathf.Min(carMoveSpeed * Time.deltaTime, targetDistance - movedDistance);
+            carTransform.position += moveDirection * moveDistance;
+            movedDistance += moveDistance;
+            yield return null;
+        }
+
+        if (carComponent != null)
+        {
+            DestroyCarAndGoline(carComponent);
+            yield break;
+        }
+
+        if (goLine != null)
+            Destroy(goLine);
+
+        if (car != null)
+            Destroy(car);
+    }
+
+    private void RegisterSpawnedCar(Car carComponent, GameObject carObject, GameObject goLineObject)
+    {
+        if (carComponent == null)
+            return;
+
+        spawnedCars[carComponent] = new SpawnedCar
+        {
+            CarObject = carObject,
+            GolineObject = goLineObject
+        };
+    }
+
+    private void OnCarHitPlayer(Car car)
+    {
+        DestroyCarAndGoline(car);
+    }
+
+    private void DestroyCarAndGoline(Car car)
+    {
+        if (car == null)
+            return;
+
+        if (spawnedCars.TryGetValue(car, out SpawnedCar spawnedCar))
+        {
+            if (spawnedCar.GolineObject != null)
+                Destroy(spawnedCar.GolineObject);
+
+            if (spawnedCar.CarObject != null)
+                Destroy(spawnedCar.CarObject);
+
+            spawnedCars.Remove(car);
+            return;
+        }
+
+        return;
     }
 
     private void SetGolineLength(Transform goLineTransform, float targetLength)
---
+++ Assets/Scripts/Traps/Car.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Car : MonoBehaviour
+{
+    public static event Action<Car> HitPlayer;
+
+    private readonly HashSet<Transform> hitPlayers = new HashSet<Transform>();
+
+    private int playerLayer;
+
+    private void Awake()
+    {
+        playerLayer = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TryHitPlayer(other);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        TryHitPlayer(collision.collider);
+    }
+
+    private void OnDisable()
+    {
+        hitPlayers.Clear();
+    }
+
+    private void TryHitPlayer(Collider other)
+    {
+        Transform playerTransform = GetPlayerTransform(other);
+        if (playerTransform == null)
+            return;
+
+        if (!hitPlayers.Add(playerTransform))
+            return;
+
+        GlobalEvents.RaiseRayHitPlayer(transform, playerTransform);
+        HitPlayer?.Invoke(this);
+    }
+
+    private Transform GetPlayerTransform(Collider other)
+    {
+        if (other == null)
+            return null;
+
+        Player player = other.GetComponentInParent<Player>();
+        if (player != null)
+            return player.transform;
+
+        PlayerOnRotatingPlatform playerOnRotatingPlatform = other.GetComponentInParent<PlayerOnRotatingPlatform>();
+        if (playerOnRotatingPlatform != null)
+            return playerOnRotatingPlatform.transform;
+
+        if (playerLayer >= 0)
+        {
+            if (other.gameObject.layer == playerLayer)
+                return other.transform;
+
+            if (other.transform.root.gameObject.layer == playerLayer)
+                return other.transform.root;
+        }
+
+        return null;
+    }
+}
